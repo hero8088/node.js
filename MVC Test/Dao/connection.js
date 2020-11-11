ccb56f5748b6f697351965aa9c4117ade8be7872
hero8088 <hero8088@naver.com>
2020-11-12 08:49:24 +0900

    sss

---
+++ MVC Test/Dao/connection.js
@@ -0,0 +1,21 @@
+const pg = require('pg');
+
+const config = {
+  host: 'localhost',
+  // Do not hard code your username and password.
+  // Consider using Node environment variables.
+  user: 'hero8088',
+  password: '8088',
+  database: 'mydb',
+  port: 5432,
+  ssl: true
+};
+
+const client = new pg.Client(config);
+
+client.connect(err => {
+  if (err) throw err;
+  else {
+    console.log("connenct success!!!");
+  }
+});
