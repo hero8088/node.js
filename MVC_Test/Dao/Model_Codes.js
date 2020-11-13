5e30b504a2a7bbf368ba5123155ede29bc3d0e00
hero8088 <hero8088@naver.com>
2020-11-13 11:27:02 +0900

    ffg

--- MVC_Test/Dao/Model_Codes.js
+++ MVC_Test/Dao/Model_Codes.js
@@ -6,27 +6,27 @@ class Model_Codes {
     this.db = PgPromise('postgres://hero8088:8088@localhost:5432/mydb');
   }
   
-  create(param) {
+  async create(param) {
     let cd = param.cd;
     let cddsc = param.cddsc; 
     return this.db.any('INSERT INTO CODES VALUES($1, $2)', [cd, cddsc]);
   }
   
-  readAll() {
+  async readAll() {
     return this.db.any('SELECT * FROM CODES');
   }
   
-  read(cd) {
+  async read(cd) {
     return this.db.any('SELECT * FROM CODES WHERE CD = $1', [cd]);
   }
   
-  update(param) {
+  async update(param) {
     let cd = param.cd;
     let cddsc = param.cddsc;
     return this.db.any('UPDATE CODES SET CDDSC = $1 WHERE CD = $2', [cddsc, cd]);
   }
   
-  delete(id) {
+  async delete(id) {
     return this.db.any('DELETE FROM CODES WHERE ID = $1', [id]);
   }
   
