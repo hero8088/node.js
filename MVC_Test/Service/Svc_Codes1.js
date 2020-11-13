1bcaf58b2640bcad74f8800b4eeedcf12443698e
hero8088 <hero8088@naver.com>
2020-11-13 10:07:23 +0900

    dd

--- MVC_Test/Service/Svc_Codes1.js
+++ MVC_Test/Service/Svc_Codes1.js
@@ -4,6 +4,6 @@ const MdCodes =  require('../Dao/Model_Codes.js');
 module.exports = {
   selectAll: function(){
     console.log('this is function');
-    console.log( MdCodes.readAll );
+    console.log( MdCodes.readAll() );
   }
 }
