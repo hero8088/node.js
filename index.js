334b64a0baed957a53139e12b4852a28aa6a7b5b
hero8088 <hero8088@naver.com>
2020-11-13 14:27:52 +0900

    sdss

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const testService = require('./MVC_Test/Service/Svc_Codes.js');
 
 var ts = new testService();
 
-ts.selectCodeList.then((data)=>{console.log(dat)});
+ts.selectCodeList.then((data)=>{console.log(data)});
