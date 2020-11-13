8f921ce6c1e3979052d20a2f47dca786cef05b92
hero8088 <hero8088@naver.com>
2020-11-13 15:09:39 +0900

    fff

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const testService = require('./MVC_Test/Service/Svc_Codes.js');
 
 var ts = new testService();
 
-ts.selectCodeList.then(function(data){
-  console.log(data);
-});
+
+async function selectCodesAll() {
+  let data = ts.selectCodeList();
+  console.dir(data);
+}
+
+ selectCodesAll();
