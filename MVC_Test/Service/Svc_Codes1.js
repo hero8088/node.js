0a671d083b2a84f9164a16e84099638be153a475
hero8088 <hero8088@naver.com>
2020-11-16 15:11:56 +0900

    ddd

--- MVC_Test/Service/Svc_Codes1.js
+++ MVC_Test/Service/Svc_Codes1.js
@@ -7,19 +7,20 @@ class ServiceCodes {
 		this.vDao = new MdCodes();
 	}
 	
-	async selectCodeList() {
+	selectCodeList() {
 		var data1;
-		await this.vDao.readAll().then(function(result){
+		this.vDao.readAll().then(function(result){
 		  console.log('success :'+result);
 		  data1=result;
 		}).catch(function(err){
 		  console.log(err);
 		});
 		console.log("마지막:"+data1);
-		return data1;
+		//return data1;
 	}
 	
 	
 }
 
-module.exports = ServiceCodes;
+new ServiceCodes().selectCodeList();
+//module.exports = ServiceCodes;
