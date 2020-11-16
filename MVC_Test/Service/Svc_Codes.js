154891b8aa9625ad045b4f82023589ef25f1bc5c
hero8088 <hero8088@naver.com>
2020-11-16 13:36:10 +0900

    hh

--- MVC_Test/Service/Svc_Codes.js
+++ MVC_Test/Service/Svc_Codes.js
@@ -3,7 +3,11 @@ const MdCodes =  require('../Dao/Model_Codes.js');
 const vDao = new MdCodes();
  
  async function myService() {
+     
    let data1 =	await vDao.readAll();
 		console.log("service:"+data1);
+		return data1;
  }
-myService();		
+myService().then(function(result){
+   console.log(result); 
+});		
