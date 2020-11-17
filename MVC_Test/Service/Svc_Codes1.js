ba4f272eee3a302d0b09dabb1c2e0857e1f8b1e0
hero8088 <hero8088@naver.com>
2020-11-17 09:33:39 +0900

    ss

--- MVC_Test/Service/Svc_Codes1.js
+++ MVC_Test/Service/Svc_Codes1.js
@@ -7,13 +7,13 @@ class ServiceCodes {
 		this.vDao = new MdCodes();
 	}
 	
-	selectCodeList() {
+	async selectCodeList() {
 	  //let sObj = this.vDao.readAll();
 	  //console.log('svc');
 	 // console.log(sObj);
 	  //return sObj;
 
-		this.vDao.readAll().then(async function(result){
+		await this.vDao.readAll().then(async function(result){
       let rslt = await result;
       console.log('service :'+result[0].cd);
 		  return rslt;
