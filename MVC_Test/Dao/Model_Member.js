a3d9f7472a923c65bfd365e83f79cdeab0ec2540
hero8088 <hero8088@naver.com>
2020-11-18 08:58:21 +0900

    fg

--- MVC_Test/Dao/Model_Member.js
+++ MVC_Test/Dao/Model_Member.js
@@ -6,15 +6,15 @@ class Model_Codes {
     this.db = PgPromise('postgres://hero8088:8088@localhost:5432/mydb');
   }
 
-  async create(param) {
+  create(param) {
     let mid = param.mid;
     let mpass = param.mpass;
     let mname = param.mname;
     let memail = param.memail;
     let mphone = param.mphone;
     let mnick = param.mnick;
-    
-    return this.db.any('INSERT INTO MEMBER (MID, MPASS, MANME, MEMAIL, MPHONE, MNICK) VALUES($1, $2, $3, $4, $5, $6)', [mid, mpass, mname, memail, mphoe, mnick]);
+    console.log(param);  
+    return this.db.none('INSERT INTO MEMBER (MID, MPASS, MANME, MEMAIL, MPHONE, MNICK) VALUES($1, $2, $3, $4, $5, $6)', [mid, mpass, mname, memail, mphoe, mnick]);
   }
 
   readAll() {
@@ -23,11 +23,11 @@ class Model_Codes {
     return obj;
   }
   
-  async read(mid) {
+  read(mid) {
     return this.db.any('SELECT * FROM MEMBER WHERE CD = $1', [mid]);
   }
 
-  async update(param) {
+  update(param) {
     let mid = param.mid;
     let mpass = param.mpass;
     let mname = param.mname;
@@ -37,7 +37,7 @@ class Model_Codes {
     return this.db.any('UPDATE MEMBER SET MPASS = $1, MNAME = $2, MEMAIL = $3, MPHONE = $4, MNICK $5 WHERE CD = $6', [ mpass, mname, memail, mphoe, mnick, mid]);
   }
 
-  async delete(mid) {
+  delete(mid) {
     return this.db.any('DELETE FROM MEMBER WHERE MID = $1', [mid]);
   }
 
