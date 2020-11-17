d0302f2ef05c64034cf075ef556c198cb19c9ca0
hero8088 <hero8088@naver.com>
2020-11-17 09:00:18 +0900

    sds

--- MVC_Test/Service/Svc_Codes1.js
+++ MVC_Test/Service/Svc_Codes1.js
@@ -8,18 +8,18 @@ class ServiceCodes {
 	}
 	
 	selectCodeList() {
-	  let sObj = this.vDao.readAll();
-	  console.log('svc');
-	  console.log(sObj);
-	  return sObj;
-	/*	var data1 = {};
+	  //let sObj = this.vDao.readAll();
+	  //console.log('svc');
+	 // console.log(sObj);
+	  //return sObj;
+		var data1 = {};
 		await this.vDao.readAll().then(function(result){
 		  console.log('service :'+result);
 		  return result;
 		}).catch(function(err){
 		  console.log(err);
 		  return err;
-		});*/
+		});
 	}
 	
 	
