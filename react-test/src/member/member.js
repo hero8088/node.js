6001210b873d666c1a8b9fa5c26a85b45f91be2d
hero8088 <hero8088@naver.com>
2020-11-20 15:23:42 +0900

    fdsfsd

--- react-test/src/member/member.js
+++ react-test/src/member/member.js
@@ -3,19 +3,25 @@
 	class Member extends Component {
 	  constructor(props) {
 	    super(props);
-	    this.state = {};
+	    this.state = {
+	      retValue: []
+	    };
 	  }
 
 	  componentWillMount() {
 	    fetch("http://localhost:3030/member")
+	    .then(res => res.json())
+	    .then(data => this.setState({
+	      retValue: data
+	    }));
 	  }
 
 	  render() {
 	    console.log('--------------------------------------');
-	    console.dir(this.stats);
+	    console.dir(this.stats.retValue);
 	    return (
 	      <div>
-					<h1>{this.state[0]}</h1>
+					<h1>{this.state.retValue}</h1>
 				</div>
 	    );
 	  }
