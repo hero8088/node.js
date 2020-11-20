574f558c53c971f3ac65cbf2bd9aa72cb62b0d52
hero8088 <hero8088@naver.com>
2020-11-20 11:13:38 +0900

    dsfds

--- react-test/src/member/clock.js
+++ react-test/src/member/clock.js
@@ -2,7 +2,7 @@
 			    render() {
 			      return (
 			        <div className = "shopping-list">
-						<h1>Shopping List for {this.props.name}</h1>
+						<h1>Shopping List </h1>
 						<ul>
 							<li>Instagram</li>
 							<li>WhatsApp</li>
