6a547604ab78d32cdc10425f82d7ae2a3868a019
hero8088 <hero8088@naver.com>
2020-11-20 10:16:34 +0900

    dssdf

---
+++ react-test/src/member/clock.js
@@ -0,0 +1,33 @@
+
+class Clock extends React.Component {
+	  constructor(props) {
+	    super(props);
+	    this.state = { ate: new Date() };
+	  }
+
+	  componentDidMount() {
+	    this.timerID = setInterval(
+	      () => this.tick(), 1000
+	    );
+	  }
+
+	  componentWillUnmount() {
+	    clearInterval(this.timerID);
+	  }
+
+	  tick() {
+	    this.setState({
+	      ate: new Date()
+	    });
+	  }
+
+	  render() {
+	    return (
+	      <div>
+					<h1>It is {this.state.ate.toLocaleTimeString()}.</h1>
+				</div>
+	    );
+	  }
+	}
+
+export default Clock;
