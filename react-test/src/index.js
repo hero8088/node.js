fe7c3723f391244564e289fa5c4347581ad0b727
hero8088 <hero8088@naver.com>
2020-11-20 13:40:33 +0900

    sddd

--- react-test/src/index.js
+++ react-test/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import Clock from './member/clock'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Clock />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
